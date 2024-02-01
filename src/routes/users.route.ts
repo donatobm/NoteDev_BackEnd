a5c9a525789663c34cd30f100cab03e320d129c7
donatobm <93447434+donatobm@users.noreply.github.com>
2024-02-01 00:42:58 -0400

    chore: add routes for editing and deleting a user

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,5 +1,11 @@
 import express from "express";
-import { createUser, loginUser, test } from "../controllers/users.controllers";
+import {
+  createUser,
+  loginUser,
+  test,
+  EditUser,
+  deleteUser,
+} from "../controllers/users.controllers";
 import passport from "passport";
 
 const router = express.Router();
@@ -7,5 +13,11 @@ const router = express.Router();
 router.post("/create", createUser);
 router.post("/login", loginUser);
 router.get("/test", passport.authenticate("jwt", { session: false }), test);
+router.put("/edit", passport.authenticate("jwt", { session: false }), EditUser);
+router.delete(
+  "/delete",
+  passport.authenticate("jwt", { session: false }),
+  deleteUser
+);
 
 export default router;
